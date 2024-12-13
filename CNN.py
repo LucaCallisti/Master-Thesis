c93b797c3e974cb5301c4a9c63c1c3cd3ff3579d
LucaCallisti <luca.callisti@gmail.com>
2024-12-13 15:55:51 +0100

    gradient of f

--- CNN.py
+++ CNN.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import Dataset
+import time
 
 class CNN(nn.Module):
     def __init__(self, input_channels, num_classes, conv_layers, size_img, pool_size=2):
@@ -32,9 +33,36 @@ class CNN(nn.Module):
         x = torch.flatten(x, 1)
         x = self.fc(x)
         return x
+    
+def count_model_parameters(model):
+    total_params = sum(param.numel() for param in model.parameters())
+    return total_params
 
+class function_f():
+    def __init__(self, model, x_train):
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def main():
+        self.model = model.to(device)
+        self.dataset = x_train.to(device)
+
+    def apply(self):
+        result = self.model(self.dataset).mean(axis=0) 
+
+        # Calculate Loss
+        loss = result.mean()
+        return loss
+    
+    def update_model_params(self, new_params):
+        self.model.load_state_dict(new_params)
+    
+    def compute_gradients(self):
+        self.model.zero_grad()
+        output = self.apply()
+        gradients = torch.autograd.grad(output.mean(), self.model.parameters(), create_graph=True)
+        gradients = torch.cat([grad.view(-1) for grad in gradients])
+        return gradients
+    
+def test():
     conv_layers = [
         (32, 3, 1, 1),  # filter number, kernel size, stride, padding
         (64, 3, 1, 1),
@@ -94,6 +122,39 @@ def main():
     print(output.shape)
 
 if __name__ == '__main__':
-    main()
+    dataset = Dataset.CIFAR10Dataset()
+    dataset.to_grayscale()
+    dataset.downscale(50)
+    num_classes = np.unique(dataset.y_train).shape[0]
+    input_channels, size_img, _ = dataset.get_image_size()
+    conv_layers = [
+        (32, 3, 1, 1),  # filter number, kernel size, stride, padding
+        (32, 3, 1, 1),
+        (32, 3, 1, 1)
+    ]
+    model = CNN(input_channels=input_channels, num_classes=num_classes, conv_layers=conv_layers, size_img=size_img)
+    total_params = count_model_parameters(model)
+    print(f"Total number of parameters: {total_params}")
+
+    f = function_f(model, dataset.x_train)
+    start = time.time()
+    aux = f.apply()
+    print(f'Elapsed time: {time.time() - start:.2f}s, calculated value: {aux:.2f}')
+
+    start = time.time()
+    grad = f.compute_gradients()
+    print(f'Elapsed time: {time.time() - start:.2f}s, gradient size: {grad.shape[0]}')
+    
+
+    model1 = CNN(input_channels=input_channels, num_classes=num_classes, conv_layers=conv_layers, size_img=size_img)
+    f.update_model_params(model1.state_dict())
+    start = time.time()
+    aux = f.apply()
+    print(f'Elapsed time: {time.time() - start:.2f}s, calculated value: {aux}')
+
+    start = time.time()
+    grad1 = f.compute_gradients()
+    print(f'Elapsed time: {time.time() - start:.2f}s, old grad != new grad: {(grad != grad1).any()}')
+
 
 
